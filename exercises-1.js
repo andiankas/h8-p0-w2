c2287e192e9daa4ba077a0d5a63fc99a16f71d4c
andiankas <rustiantoandi17@gmail.com>
2018-01-29 17:26:47 +0700

    add exercises 1

---
+++ exercises-1.js
@@ -0,0 +1,48 @@
+/** 1. NEWTON SECOND LAW
+ *
+ STORE m = 600
+ STORE a = 2
+ STORE hitung = m * a
+ DISPLAY HITUNG
+ */
+
+
+/* 2. TAHUN KABISAT
+
+MASUKKAN sebuah tahun pada `tahun`
+IF `tahun` habis dibagi 4 THEN:
+    IF `tahun` tidak habis dibagi 100 THEN:
+        TULISKAN "`tahun` adalah tahun kabisat"
+    ELSE:
+        IF `tahun` habis dibagi 400 THEN:
+            TULISKAN "`tahun` adalah tahun kabisat"
+        ELSE:
+            TULISKAN "`tahun` adalah bukan tahun kabisat"
+        ENDIF
+    ENDIF
+ELSE:
+    TULISKAN "`tahun` adalah bukan tahun kabisat"
+ENDIF
+  * 
+  */
+
+
+
+/* 3. LAUNDRY DAY
+
+STORE "full level" = 20
+
+WHILE "full level" <= 20
+  ADD "full level" by 1
+
+DISPLAY "Mesin Cuci Menyala"
+ */
+
+ 
+ /* PERIKSA KUKU
+   FOR KUKU <= 40
+   IF SISWA = KUKU PANJANG
+        DO HUKUM
+    ELSE
+        DO DIPUJI
+  */
