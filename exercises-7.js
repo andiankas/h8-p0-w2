ff01f30bde1db832928d567d8668f4e306ab6a3b
andiankas <rustiantoandi17@gmail.com>
2018-01-30 16:05:43 +0700

    finished exercises-7

--- exercises-7.js
+++ exercises-7.js
@@ -1,19 +1,19 @@
-// // 1. Menyusun Barisan Bintang
+// 1. Menyusun Barisan Bintang
 
-// var rows1 = 5; // input the number of rows
-// for(i=1;i<=rows1;i++){
-//     console.log('*')
-// }
+var rows1 = 5; // input the number of rows
+for(i=1;i<=rows1;i++){
+    console.log('*')
+}
 
-// // 2. Menyusun Barisan Bintang Dengan Nested Looping
-// var rows2 = 5;
-// for(i=1;i<=rows2;i++){
-//     var bintang = '';
-//     for(j=1;j<=rows2;j++){
-//       bintang = bintang + '*';
-//     }
-//     console.log(bintang);
-//   }
+// 2. Menyusun Barisan Bintang Dengan Nested Looping
+var rows2 = 5;
+for(i=1;i<=rows2;i++){
+    var bintang = '';
+    for(j=1;j<=rows2;j++){
+      bintang = bintang + '*';
+    }
+    console.log(bintang);
+  }
 
 // 3. Menyusun Barisan Tangga Bintang Dengan Nested Looping
 var rows3 = 5;
