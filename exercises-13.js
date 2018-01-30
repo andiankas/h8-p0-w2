7cad06bca52c3f00ef7162bc277549969c8425be
andiankas <rustiantoandi17@gmail.com>
2018-01-30 10:48:13 +0700

    add exercises 13, but unfinished

---
+++ exercises-13.js
@@ -0,0 +1,15 @@
+// Logic Challenge - X dan O
+
+function xo(str) {
+    var pecahX = str.split('x')
+    var pecahO = str.split('o')
+    
+    
+  }
+  
+  // TEST CASES
+  console.log(xo('xoxoxo')); // true
+  console.log(xo('oxooxo')); // false
+  console.log(xo('oxo')); // false
+  console.log(xo('xxxooo')); // true
+  console.log(xo('xoxooxxo')); // true
