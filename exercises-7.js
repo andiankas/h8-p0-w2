cd932472a55be2abf4e453acd2f75af19838f794
andiankas <rustiantoandi17@gmail.com>
2018-01-30 10:11:31 +0700

    add exercises 7 but unfinished

---
+++ exercises-7.js
@@ -0,0 +1,14 @@
+// // 1. Menyusun Barisan Bintang
+
+// var rows1 = 5; // input the number of rows
+// for(i=1;i<=rows1;i++){
+//     console.log('*')
+// }
+
+// 2. Menyusun Barisan Bintang Dengan Nested Looping
+var rows2 = 5;
+for(i=1;i<=rows2;i++){
+    for(j=1;j=rows2;j++){
+        console.log('*')
+    }
+}
